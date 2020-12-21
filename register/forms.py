99a6a3a2c56a47ae85742ce91e7c91971b496ccf
Bat Icho <boticho23@gmail.com>
2020-12-22 00:27:43 +0200

    add some bugs in forms

--- register/forms.py
+++ register/forms.py
@@ -1,14 +1,29 @@
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django import forms
 
+from register.models import UserProfile
 
-class RegisterForm(forms.ModelForm):
+
+class RegisterForm(UserCreationForm):
 	class Meta:
 		model = User
-		fields = ('username', 'password', 'email',)
+		fields = ('username', 'email',)
+
+	def clean_email(self):
+		email = self.cleaned_data.get('email', False)  # if have email return it else return False
+		if not email:
+			raise forms.ValidationError('Email is required')
+		return email
 
 
-class LoginForm(forms.ModelForm):
+class LoginForm(AuthenticationForm):
 	class Meta:
 		model = User
-		fields = ('username', 'password',)
+
+
+class ProfileForm(forms.ModelForm):
+	class Meta:
+		model = UserProfile
+		exclude = ('user',)
+
--- register/views.py
+++ register/views.py
@@ -1,36 +1,57 @@
 from django.contrib.auth import authenticate, login, logout
+from django.db import transaction
 from django.shortcuts import render, redirect
 
-from register.forms import RegisterForm
+from register.forms import RegisterForm, LoginForm, ProfileForm
 
 
+@transaction.atomic  # изпълнява всички или нито една
 def register_user(request):
     if request.method == 'GET':
         context = {
-            'form': RegisterForm()
+            'user_form': RegisterForm(),
+            'profile_form': ProfileForm(),
         }
         return render(request, 'register.html', context)
     else:
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-            user = form.save()  # add user
+        user_form = RegisterForm(request.POST)
+        profile_form = ProfileForm(request.POST, request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()  # add user
+            profile = profile_form.save(commit=False)  # не го запазва преди да направи връзката 1 към 1
+            profile.user = user
+            profile.save()
+
             login(request, user)  # login user
             return redirect('register user')
 
         context = {
-            'form': form
+            'user_form': RegisterForm(),
+            'profile_form': ProfileForm(),
         }
         return render(request, 'register.html', context)
 
 
 def login_user(request):
-    username = 'baticho'
-    password = 'Rali2230'
-    user = authenticate(username=username, password=password)
-    if user:
-        login(request, user)
-        return redirect('home')
-    return redirect('home')
+    if request.method == 'GET':
+        login_form = LoginForm()
+        context = {
+            'login_form': login_form
+        }
+        return render(request, 'login.html', context)
+    else:
+        login_form = LoginForm(request.POST)
+        if login_form.is_valid():
+            username = login_form.cleaned_data['username']
+            password = login_form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect('home')
+        context = {
+            'login_form': login_form
+        }
+        return render(request, 'login.html', context)
 
 
 def logout_user(request):
